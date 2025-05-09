9c081738d6b79cbbc6c9f159f108ad3762a150a8
Lennert-dot <lennert.luyten1@gmail.com>
2025-05-09 13:40:26 +0200

    stop added in timer class

--- timer.py
+++ timer.py
@@ -5,12 +5,16 @@ class Timer(Component):
     def __init__(self, max_time: int):
         self.max_time = max_time
         self.start_time = None
+        self.stop_time = None
 
     def startTime(self):
         self.start_time = datetime.now()
 
-    def get_duration(self):
+    def getDuration(self):
         return int((datetime.now() - self.start_time).total_seconds()) if self.start_time else 0
 
-    def get_remaining(self):
-        return max(0, self.max_time - self.get_duration())
+    def getRemaining(self):
+        return max(0, self.max_time - self.getDuration())
+    
+    def stopTime(self):
+        self.stop_time = datetime.now()
